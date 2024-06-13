b2abebf84508bd759e15459f3556e46a7b06a814
Siva-S92 <sivagraphics4fashion@gmail.com>
2024-06-13 17:22:33 +0530

    deploycommit

---
+++ server/api/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import databaseConnection from '../utils/database.js';
+import { userRouter } from '../routes/userRoute.js';
+
+
+//config the env variables
+dotenv.config();
+
+//server setup
+const app = express();
+const PORT = process.env.PORT;
+
+//using middlewares
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+const cors_options = {
+    origin: "http://localhost:5173",
+    credentials: true
+}
+app.use(cors(cors_options));
+
+//database Connection
+databaseConnection();
+
+
+//routes
+app.use("/api/user", userRouter );
+
+
+
+
+
+//Listern Server
+app.listen(PORT, ()=> {
+    console.log("server running on the PORT:", PORT)
+})
+
+// export { app };
